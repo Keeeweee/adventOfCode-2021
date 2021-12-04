4665a42d77244c1f0940aa4f045fec137e05656f
Keewee <epsilonmajorquezero@gmail.com>
2021-12-05 00:24:58 +0100

    Add Day 02

---
+++ day-02/first.py
@@ -0,0 +1,16 @@
+instructions = [line.rstrip('\n').split(' ') for line in open('data/data.txt')]
+
+print(instructions)
+
+
+position = [0, 0]
+
+for instruction in instructions:
+    if instruction[0] == 'forward':
+        position[0] += int(instruction[1])
+    elif instruction[0] == 'down':
+        position[1] += int(instruction[1])
+    elif instruction[0] == 'up':
+        position[1] -= int(instruction[1])
+
+print(position[0] * position[1])
